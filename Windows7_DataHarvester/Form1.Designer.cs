c35847c7d2b1cd021e64cdf9016ea621883861f8
Jesse Clark <hogarth45@gmail.com>
2015-12-23 01:20:25 -0600

    adding files
    files are needed

---
+++ Windows7_DataHarvester/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Windows7_DataHarvester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._btnStart = new System.Windows.Forms.Button();
+            this._rtbLogs = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this._btnBrowse = new System.Windows.Forms.Button();
+            this._txtPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // _btnStart
+            // 
+            this._btnStart.Location = new System.Drawing.Point(652, 12);
+            this._btnStart.Name = "_btnStart";
+            this._btnStart.Size = new System.Drawing.Size(75, 49);
+            this._btnStart.TabIndex = 0;
+            this._btnStart.Text = "Start";
+            this._btnStart.UseVisualStyleBackColor = true;
+            this._btnStart.Click += new System.EventHandler(this._btnStart_Click);
+            // 
+            // _rtbLogs
+            // 
+            this._rtbLogs.Location = new System.Drawing.Point(13, 71);
+            this._rtbLogs.Name = "_rtbLogs";
+            this._rtbLogs.Size = new System.Drawing.Size(714, 209);
+            this._rtbLogs.TabIndex = 1;
+            this._rtbLogs.Text = "";
+            // 
+            // _btnBrowse
+            // 
+            this._btnBrowse.Location = new System.Drawing.Point(13, 12);
+            this._btnBrowse.Name = "_btnBrowse";
+            this._btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this._btnBrowse.TabIndex = 2;
+            this._btnBrowse.Text = "Browse";
+            this._btnBrowse.UseVisualStyleBackColor = true;
+            this._btnBrowse.Click += new System.EventHandler(this._btnBrowse_Click);
+            // 
+            // _txtPath
+            // 
+            this._txtPath.Location = new System.Drawing.Point(13, 41);
+            this._txtPath.Name = "_txtPath";
+            this._txtPath.Size = new System.Drawing.Size(333, 20);
+            this._txtPath.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 292);
+            this.Controls.Add(this._txtPath);
+            this.Controls.Add(this._btnBrowse);
+            this.Controls.Add(this._rtbLogs);
+            this.Controls.Add(this._btnStart);
+            this.Name = "Form1";
+            this.Text = "DataHarvester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _btnStart;
+        private System.Windows.Forms.RichTextBox _rtbLogs;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button _btnBrowse;
+        private System.Windows.Forms.TextBox _txtPath;
+    }
+}
+
---
+++ Windows7_DataHarvester/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows7_DataHarvester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            _txtPath.Text = @"C:\Users\hogarth45\Documents\dock";
+        }
+
+        private void _btnStart_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(_txtPath.Text))
+            {
+                MessageBox.Show("Please select a folder to store the information gathered.");
+                return;
+            }
+
+            if (!Directory.Exists(_txtPath.Text))
+            {
+                MessageBox.Show("The selected folder does not exist.");
+                return;
+            }
+
+            CopyFile("Thumbnail cache", @"C:\Users\%username%\AppData\Local\Microsoft\Windows\Explorer", _txtPath.Text);
+            CopyFile("Wifi Passwords", @"C:\ProgramData\Microsoft\Wlansvc\Profiles\Interfaces", _txtPath.Text);
+            CopyFile("Hibernation File", @"C:\hiberfil.sys", _txtPath.Text);
+            CopyFile("Spool Files", @"%SystemRoot%\SYSTEM32\SPOOL\PRINTERS", _txtPath.Text);
+            CopyFile("Prefetch", @"%SystemRoot%\Prefetch", _txtPath.Text);
+            CopyFile("Firefox", @"%APPDATA%\Mozilla\Firefox\Profiles", _txtPath.Text);
+            CopyFile("USB History", @"%SystemRoot%\inf\setupapi.dev.log", _txtPath.Text);
+            CopyFile("Hibernation File", @"%SystemRoot%\System32\config\System", _txtPath.Text);
+        }
+
+        private void _btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+                _txtPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void CopyFile(string name, string targetPath, string homePath)
+        {
+            Message("Attempting to move " + name);
+            string[] targetPathParts = targetPath.Split('\\');
+
+            for(int x =0; x<targetPathParts.Length;x++)
+            {
+                if (targetPathParts[x].Contains('%'))
+                    targetPathParts[x] = Environment.GetEnvironmentVariable(targetPathParts[x].Trim('%'));
+                else if (targetPathParts[x].Contains(':'))
+                    targetPathParts[x] = targetPathParts[x].Insert(targetPathParts[x].IndexOf(':') + 1, "\\");
+            }
+
+
+
+            if (!Directory.Exists(Path.Combine(targetPathParts)))
+            {
+                Message("Failed, not normal location");
+                //Message("Could not find, " + name + " located at " + targetPath);
+                return;
+            }
+
+            if (targetPathParts[targetPathParts.Length - 1].Contains('.'))
+            {
+                Directory.CreateDirectory(Path.Combine(homePath, name));
+                File.Copy(Path.Combine(targetPathParts), homePath, true);
+            }
+            else
+            {
+                Directory.CreateDirectory(Path.Combine(homePath, name));
+
+                string[] files = System.IO.Directory.GetFiles(Path.Combine(targetPathParts));
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    string fileName = System.IO.Path.GetFileName(s);
+                    string destFile = System.IO.Path.Combine(homePath, name, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+            }
+            Message(name + " Successfully moved");
+        }
+
+        private void Message(string message)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(message);
+            sb.Append('\n');
+            _rtbLogs.Text += sb.ToString();
+            _rtbLogs.Refresh();
+        }
+    }
+}
